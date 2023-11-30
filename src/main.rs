762693f4da405501fd5171fe104d759478ac1748
Gabe R. <9311953+Marsfan@users.noreply.github.com>
2023-11-29 21:46:47 -0700

    Add some error parsing. Clean up hasher a bit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use disk_compare::errors::ToolError;
 use rayon::iter::ParallelBridge;
 use rayon::prelude::ParallelIterator;
 use sha2::{Digest, Sha256};
@@ -7,7 +8,6 @@ use std::path::PathBuf;
 use walkdir::{DirEntry, Error, WalkDir};
 
 // TODO: Proper error handling
-// TODO: Argument for specifying the path to use
 // TODO: Argument for selecting the hash (SHa256, MD5, etc.)
 // TODO: Actually compare directories
 // TODO: Put on github
@@ -29,28 +29,33 @@ impl FileHash {
     ///
     /// Returns:
     ///     The created `FileHash`
-    pub fn new(filepath: PathBuf) -> Self {
-        if filepath.is_file() {
+    pub fn new(filepath: PathBuf) -> Result<Self, ToolError> {
+        let is_file = filepath.is_file();
+
+        // Only compute hash if the path points to a file
+        let hash = if is_file {
             let mut hasher = Sha256::new();
-            let mut file = File::open(&filepath).unwrap();
+            let mut file = File::open(&filepath).map_err(|error| ToolError::FileReadError {
+                kind: error.kind(),
+                filepath: filepath.display().to_string(),
+            })?;
+
             // This whole io::copy thing came from here
             // https://www.reddit.com/r/rust/comments/tuxpxf/comment/i368ryk/
             // Uses way less memory than reading the file directly
             // Guessing its sending copying the file in chunks?
-            io::copy(&mut file, &mut hasher).unwrap();
-            let hash = hasher.finalize().to_vec();
-            Self {
-                filepath,
-                hash,
-                is_file: true,
-            }
+
+            io::copy(&mut file, &mut hasher)?;
+            hasher.finalize().to_vec()
         } else {
-            Self {
-                filepath,
-                hash: Vec::new(),
-                is_file: false,
-            }
-        }
+            Vec::new()
+        };
+
+        Ok(Self {
+            filepath,
+            hash,
+            is_file,
+        })
     }
 
     /// Get the printout line for the given hash
@@ -76,7 +81,7 @@ fn main() {
         .par_bridge()
         .map(|entry: Result<DirEntry, Error>| {
             let path = PathBuf::from(entry.unwrap().path());
-            FileHash::new(path).as_print_line()
+            FileHash::new(path).unwrap().as_print_line()
         })
         .for_each(|result| println!("{result}"));
 }
